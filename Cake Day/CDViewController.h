7d6d66fd2c28d5ca54a72c73d115ad94b8dea1a9
Programming Thomas <programmingthomas@gmail.com>
2013-08-25 20:27:50 +0100

    Added shadows

--- Cake Day/CDViewController.h
+++ Cake Day/CDViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
 #import "CDUtility.h"
 #import "CDUserListViewController.h"
 #import "CDCakeViewController.h"
--- Cake Day/CDViewController.m
+++ Cake Day/CDViewController.m
@@ -24,6 +24,7 @@
     self.menuVisible = YES;
     UITapGestureRecognizer * tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapOnCakeViewController:)];
     [self.cakeViewContainer addGestureRecognizer:tapGesture];
+    [self shadowsAndCorners:YES];
 }
 
 - (void)didReceiveMemoryWarning
@@ -38,6 +39,7 @@
     {
         [UIView animateWithDuration:0.25f animations:^{
             self.cakeViewContainer.frame = self.view.bounds;
+            [self shadowsAndCorners:NO];
         } completion:^(BOOL finished) {
             self.menuVisible = NO;
         }];
@@ -50,10 +52,18 @@
     {
         [UIView animateWithDuration:0.25f animations:^{
             self.cakeViewContainer.frame = CGRectMake(240, 0, CGRectGetHeight(self.view.bounds), CGRectGetHeight(self.view.bounds));
+            [self shadowsAndCorners:YES];
         } completion:^(BOOL finished) {
             self.menuVisible = YES;
         }];
     }
+}
+
+-(void)shadowsAndCorners:(BOOL)enabled
+{
+    self.cakeViewContainer.layer.shadowColor = [UIColor blackColor].CGColor;
+    self.cakeViewContainer.layer.shadowOpacity = enabled ? 0.5f : 0;
+    self.cakeViewContainer.layer.shadowOffset = CGSizeMake(-5, 0);
 }
 
 @end
