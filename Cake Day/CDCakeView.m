94a797fe8dc2ea5173615717d15213cfe5fd45e2
Programming Thomas <programmingthomas@gmail.com>
2013-08-28 17:29:27 +0100

    Improved candle layout algorithm

--- Cake Day/CDCakeView.m
+++ Cake Day/CDCakeView.m
@@ -27,18 +27,38 @@
     
     CakeDrawingFunction(context, rect);
     
-    float distanceBetweenCandles = 190.0f / (float)_candles;
-    float leftMostCandle = -distanceBetweenCandles * (float)_candles / 2.0f;
+//    float distanceBetweenCandles = 190.0f / (float)_candles;
+//    float leftMostCandle = -distanceBetweenCandles * (float)_candles / 2.0f;
+//    
+//    for (int n = 0; n < _candles; n++)
+//    {
+//        float offset = 0, yOffset = 0;
+//        if (_candles > 1)
+//        {
+//            offset = ((float)n ) / ((float)(_candles-1) / 2.0f) * M_PI / 2.0f;
+//            yOffset = sinf(offset);
+//        }
+//        CGRect offsetRect = CGRectOffset(rect, leftMostCandle + ((float)n + 0.5f) * distanceBetweenCandles, yOffset * 10);
+//        CandleDrawingFunction(context, offsetRect);
+//    }
     
     for (int n = 0; n < _candles; n++)
     {
-        float offset = 0, yOffset = 0;
-        if (_candles > 1)
+        float radius = 1;
+        if (_candles == 1)
         {
-            offset = ((float)n ) / ((float)(_candles-1) / 2.0f) * M_PI / 2.0f;
-            yOffset = sinf(offset);
+            radius = 0;
         }
-        CGRect offsetRect = CGRectOffset(rect, leftMostCandle + ((float)n + 0.5f) * distanceBetweenCandles, yOffset * 10);
+        else if (_candles == 2)
+        {
+            radius = 0.5;
+        }
+        float angle = (float)n / (float)_candles * 2.0f * M_PI;
+        float circleX = cosf(angle) * radius;
+        float circleY = -sinf(angle) / 3 * radius;
+        float x = circleX * 0.6 * CGRectGetMidX(rect);
+        float y = circleY * 0.6 * CGRectGetMidY(rect);
+        CGRect offsetRect = CGRectOffset(rect, x, y);
         CandleDrawingFunction(context, offsetRect);
     }
 }
