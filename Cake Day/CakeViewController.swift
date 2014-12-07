68de0df44e80cd0b692dbc1d338aef23bfa73fb5
Programming Thomas <programmingthomas@gmail.com>
2014-12-07 08:41:17 +0000

    Create bridging header and cake view controller

---
+++ Cake Day/CakeViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CakeViewController.swift
+//  Cake Day
+//
+//  Created by Thomas Denney on 07/12/2014.
+//  Copyright (c) 2014 Thomas Denney. All rights reserved.
+//
+
+import UIKit
+
+class CakeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
