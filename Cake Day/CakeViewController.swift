0134050c43644069a1022a6ae5b4e833dc9515e1
Programming Thomas <programmingthomas@gmail.com>
2014-12-07 11:17:41 +0000

    Fully ported CakeViewController

--- Cake Day/CakeViewController.swift
+++ Cake Day/CakeViewController.swift
@@ -8,13 +8,15 @@
 
 import UIKit
 
-class CakeViewController: UIViewController {
+class CakeViewController: UIViewController, UIActionSheetDelegate {
 
     @IBOutlet weak var shareButton: UIBarButtonItem!
     @IBOutlet weak var cakeView: CDCakeView!
     @IBOutlet weak var countdownLabel: UILabel!
     
     var user: User?
+    
+    var shareActionSheet: UIActionSheet?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,6 +31,47 @@ class CakeViewController: UIViewController {
     }
 
     @IBAction func shareTapped(sender: AnyObject) {
+        let title = NSLocalizedString("share", comment: "")
+        let shareText = NSLocalizedString("share.text", comment: "")
+        let shareCake = NSLocalizedString("share.cake", comment: "")
+        let cancel = NSLocalizedString("cancel", comment: "")
+        
+        let actionSheet = UIActionSheet(title: title, delegate: self, cancelButtonTitle: cancel, destructiveButtonTitle: nil)
+        actionSheet.addButtonWithTitle(shareText)
+        actionSheet.addButtonWithTitle(shareCake)
+        
+        shareActionSheet = actionSheet
+        
+        actionSheet.showFromBarButtonItem(self.shareButton, animated: true)
+    }
+    
+    func actionSheet(actionSheet: UIActionSheet, clickedButtonAtIndex buttonIndex: Int) {
+        if actionSheet == shareActionSheet {
+            let formatString = NSLocalizedString("share.string", comment: "");
+            let interval = CDUtility.durationString(user!.timeIntervalToNextCakeDay());
+            let usernameWithApostrophe = user!.usernameWithApostrophe;
+            
+            let shareString = NSString(format:formatString, [interval, usernameWithApostrophe]);
+            
+            if buttonIndex == 1 {
+                let shareController = UIActivityViewController(activityItems: [shareString], applicationActivities: nil)
+                presentViewController(shareController, animated: true, completion: nil)
+            }
+            else if buttonIndex == 2 {
+                NSOperationQueue().addOperationWithBlock {
+                    //Good size for Instagram
+                    UIGraphicsBeginImageContextWithOptions(CGSizeMake(612, 612), false, 0);
+                    self.cakeView.drawRect(CGRect(x: 0, y: 0, width: 612, height: 612));
+                    let image = UIGraphicsGetImageFromCurrentImageContext();
+                    UIGraphicsEndImageContext();
+                    
+                    NSOperationQueue.mainQueue().addOperationWithBlock {
+                        let shareController = UIActivityViewController(activityItems: [shareString, image], applicationActivities: nil)
+                        self.presentViewController(shareController, animated: true, completion: nil)
+                    }
+                }
+            }
+        }
     }
 
     func updateTimer() {
